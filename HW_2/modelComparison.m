5a1b9c77931751403a4cde1a9b0dc078e6226b33
Nick Agtual <nick.agtual@icloud.com>
2022-10-23 14:09:24 -0700

    Added Voigt model to calculate mechanical properties

--- HW_2/modelComparison.m
+++ HW_2/modelComparison.m
@@ -3,60 +3,77 @@ function [properties] = modelComparison(Ef, vf, cf, Em, vm , cm)
 % Defining all models compared in this subroutine
 models = {'Voigt', 'Reuss', 'Hybrid', 'Square Fiber', 'Halpin-Tsai'};
 
+functions = {@voigt, @reuss, @hybrid, @SFM, @halpinTsai};
+
 % Populating the structure with all of the models
 for ii = 1:length(models)
     properties(ii).model = models{ii};
     
-end
-
-for ii = 1:length(models)
-    switch ii
-        case 1
-            voigt(Ef, vf, cf, Em, vm , cm)
-        case 2
-            z = 1;
-        case 3
-            [properties(ii).E1, ...
-             properties(ii).E2, ...
-             properties(ii).v12, ...
-             properties(ii).G12] = hybrid(Ef, vf, cf, Em, vm, cm);
-        case 4
-            [properties(ii).E1, ...
-             properties(ii).E2, ...
-             properties(ii).v12, ...
-             properties(ii).G12] = SFM(Ef, vf, cf, Em, vm, cm);
-        case 5
-            z = 1;
-    end
+    [properties(ii).E1, ...
+     properties(ii).E2, ...
+     properties(ii).v12, ...
+     properties(ii).G12] = functions{ii}(Ef, vf, cf, Em, vm , cm);
+    
 end
 
     % Voigt model
     function [E1, E2, v12, G12] = voigt(Ef, vf, cf, Em, vm , cm)
         
-                % Plain reduced stiffness matrix
-        C = [Em / (1 - (vm ^ 2)), (vm * Em) / (1 - (vm ^ 2)), ...
+        % Function for estimated shear modulus
+        G = @(E, v) E / (2 * (1 + v));
+        
+        % Estimated fiber shear moudulus
+        Gf = G(Ef, vf);
+        
+        % Estimated matrix shear modulus
+        Gm = G(Em, vm);
+        
+        % Stifness matrix for matrix
+        C.m = [Em / (1 - (vm ^ 2)), (vm * Em) / (1 - (vm ^ 2)), ...
               0; (vm * Em) / (1 - (vm ^ 2)), Em / (1 - (vm ^ 2)), ...
               0; 0, 0, Gm];
           
-        S = inv(C)
+        % Stiffness matrix for fibers
+        C.f = [Ef / (1 - (vf ^ 2)), (vf * Ef) / (1 - (vf ^ 2)), ...
+              0; (vf * Ef) / (1 - (vf ^ 2)), Ef / (1 - (vf ^ 2)), ...
+              0; 0, 0, Gf];
         
+        % Stiffness matrix according to Voigt
+        C.v = (cf .* C.f) + (cm .* C.m);
         
-     
+        % Compliance matrix according to Voigt
+        S.v = inv(C.v);
+        
+        % Voigt longitudinal modulus
+        E1 = 1 / S.v(1, 1);
+        
+        % Voigt transverse modulus
+        E2 = 1 / S.v(2, 2);
+        
+        % Voigt major Poisson's ratio
+        v12 = -S.v(2, 1) * E1;
+        
+        % Voigt shear modulus
+        G12 = 1 /S.v(3, 3);
+        
     end
 
     function [E1, E2, v12, G12] = reuss(Ef, vf, cf, Em, vm , cm)
         
-        x = 1;
+        E1 = 1;
+        E2 = 1;
+        v12 = 1;
+        G12 = 1;
         
     end
 
     % Hybrid model
     function [E1, E2, v12, G12] = hybrid(Ef, vf, cf, Em, vm , cm)
         
-        % Longitudinal modulus
+        % Hybirid longitudinal modulus
         E1 = (cf * Ef) + (cm * Em);
         
-        % Transverse modulus
+        % Hybrid transverse modulus
         E2 = 1 / (((cf / Ef) + (cm / Em)) - (((cf * cm) / (Ef * Em)) * ...
             ((((vf * Em) - (vm * Em)) ^ 2) / ((cm * Em) + (cf * Ef)))));
         
@@ -69,10 +86,10 @@ end
         % Estimated matrix shear modulus
         Gm = G(Em, vm);
         
-        % In-palne shear modulus
+        % Hybrid in-palne shear modulus
         G12 = 1 / ((cf * (1 / Gf)) + (cm * (1 / Gm)));
         
-        % Major Poisson's ratio
+        % Hybrid major Poisson's ratio
         v12 = (cf * vf) + (cm * vm);
         
     end
@@ -139,7 +156,10 @@ end
 
     function [E1, E2, G12, v12] = halpinTsai(Ef, vf, cf, Em, vm , cm)
         
-        x = 1;
+        E1 = 1;
+        E2 = 1;
+        v12 = 1;
+        G12 = 1;
         
     end
 
