7263d4f10f82c2dc6a638bdecd73dc7bd61fecad
Nick Agtual <nick.agtual@icloud.com>
2022-10-23 13:46:50 -0700

    Added Square fiber model to compute mechanical properties

--- HW_2/modelComparison.m
+++ HW_2/modelComparison.m
@@ -9,28 +9,56 @@ for ii = 1:length(models)
     
 end
 
+for ii = 1:length(models)
+    switch ii
+        case 1
+            voigt(Ef, vf, cf, Em, vm , cm)
+        case 2
+            z = 1;
+        case 3
+            [properties(ii).E1, ...
+             properties(ii).E2, ...
+             properties(ii).v12, ...
+             properties(ii).G12] = hybrid(Ef, vf, cf, Em, vm, cm);
+        case 4
+            [properties(ii).E1, ...
+             properties(ii).E2, ...
+             properties(ii).v12, ...
+             properties(ii).G12] = SFM(Ef, vf, cf, Em, vm, cm);
+        case 5
+            z = 1;
+    end
+end
 
     % Voigt model
-    function [E1, E2, G12, v12] = voigt(Ef, vf, cf, Em, vm , cm)
+    function [E1, E2, v12, G12] = voigt(Ef, vf, cf, Em, vm , cm)
         
-        x = 1;
+                % Plain reduced stiffness matrix
+        C = [Em / (1 - (vm ^ 2)), (vm * Em) / (1 - (vm ^ 2)), ...
+              0; (vm * Em) / (1 - (vm ^ 2)), Em / (1 - (vm ^ 2)), ...
+              0; 0, 0, Gm];
+          
+        S = inv(C)
+        
+        
      
     end
 
-    function [E1, E2, G12, v12] = reuss(Ef, vf, cf, Em, vm , cm)
+    function [E1, E2, v12, G12] = reuss(Ef, vf, cf, Em, vm , cm)
         
         x = 1;
         
     end
 
     % Hybrid model
-    function [E1, E2, G12, v12] = hybrid(Ef, vf, cf, Em, vm , cm)
+    function [E1, E2, v12, G12] = hybrid(Ef, vf, cf, Em, vm , cm)
         
         % Longitudinal modulus
-        E1 = (cf * Ef) * (cm * Em);
+        E1 = (cf * Ef) + (cm * Em);
         
         % Transverse modulus
-        E2 = (cf / Ef) + ((cm / Em) * (1 - (vm ^ 2)));
+        E2 = 1 / (((cf / Ef) + (cm / Em)) - (((cf * cm) / (Ef * Em)) * ...
+            ((((vf * Em) - (vm * Em)) ^ 2) / ((cm * Em) + (cf * Ef)))));
         
         % Function for estimated shear modulus
         G = @(E, v) E / (2 * (1 + v));
@@ -42,14 +70,18 @@ end
         Gm = G(Em, vm);
         
         % In-palne shear modulus
-        G12 = (cf * (1 / Gf)) + (cm * (1 / Gm));
+        G12 = 1 / ((cf * (1 / Gf)) + (cm * (1 / Gm)));
         
         % Major Poisson's ratio
         v12 = (cf * vf) + (cm * vm);
         
     end
 
-    function [E1, E2, G12, v12] = sfm(Ef, vf, cf, Em, vm , cm)
+    function [E1, E2, v12, G12] = SFM(Ef, vf, cf, Em, vm , cm)
+        
+        % Modified volume ratios
+        cA = sqrt(cf);
+        cB = 1 - cA;
         
         % Function for estimated shear modulus
         G = @(E, v) E / (2 * (1 + v));
@@ -57,14 +89,52 @@ end
         % Estimated matrix shear modulus
         Gm = G(Em, vm);
         
+        % Estimated fiber shear moudulus
+        Gf = G(Ef, vf);
+        
         % Plain reduced stiffness matrix
         Q.m = [Em / (1 - (vm ^ 2)), (vm * Em) / (1 - (vm ^ 2)), ...
               0; (vm * Em) / (1 - (vm ^ 2)), Em / (1 - (vm ^ 2)), ...
               0; 0, 0, Gm];
+          
+        % Hybrid in-plane modulus
+        hybrid.E1 = (cA * Ef) + (cB * Em);
+        
+        % Hybrid major Poisson's ratio
+        hybrid.v12 = (cA * vf) + (cB * vm);
+        
+        % Hybrid shear modulus
+        hybrid.G12 = 1 / ((cA / Gf) + (cB / Gm));
+        
+        % Hybrid transverse modulus
+        hybrid.E2 = (cA / Ef) + ((cB / Em) * (1 - (vm ^ 2)));
+        
+        % Hybrid minor Poisson's ratio 
+        hybrid.v21 = (hybrid.v12 * hybrid.E2) / hybrid.E1;
         
         % Hybrid reduced stiffness matrix
-        Q.h = 1;
+        Q.h = [hybrid.E1 / (1 - (hybrid.v12 * hybrid.v21)), ...
+            (hybrid.v12 * hybrid.E2) / (1 - (hybrid.v12 * hybrid.v21)), ...
+            0; (hybrid.v12 * hybrid.E2) / ...
+            (1 - (hybrid.v12 * hybrid.v21)), hybrid.E2 / ...
+            (1 - (hybrid.v12 * hybrid.v21)), 0; 0, 0, hybrid.G12];
         
+        % Reduced stiffness matrix according to SFM
+        Q.SFM = (cA .* Q.h) + (cB .* Q.m);
+        
+        % SFM longitudinal modulus
+        E1 = ((Q.SFM(1, 1) * Q.SFM(2, 2)) - (Q.SFM(1, 2) ^ 2)) / ...
+            Q.SFM(2, 2);
+        
+        % SFM transverse modulus
+        E2 = ((Q.SFM(1, 1) * Q.SFM(2, 2)) - (Q.SFM(1, 2) ^ 2)) / ...
+            Q.SFM(1, 1);
+        
+        % SFM shear modulus
+        G12 = Q.SFM(3, 3);
+        
+        % SFM major Poisson's ratio
+        v12 = Q.SFM(1, 2) / Q.SFM(2, 2);
     end
 
     function [E1, E2, G12, v12] = halpinTsai(Ef, vf, cf, Em, vm , cm)
