e75d9f097eded5f4ae891e1c6b25d7f7c3689a40
Nick Agtual <nick.agtual@icloud.com>
2022-12-02 15:57:14 -0800

    Test code for hygrothermal effects

--- Final Project/hygrothermalEffetcs.m
+++ Final Project/hygrothermalEffetcs.m
@@ -4,21 +4,15 @@ function [hygrothermal] = ...
 % Temperature difference
 hygrothermal.deltaT = hygrothermal.Tf - hygrothermal.T0;
 
-% Moisture difference
-hygrothermal.deltaC = hygrothermal.Cf - hygrothermal.C0;
-
-
-%% Local Hygrothermal Properties & Free Strain
+%% Global Hygrothermal Properties & Free Strain
 for ii = 1:length(ss)
     
-    % Local hygrothermal properties
+    % Global hygrothermal properties
     hygrothermal.alphaLocal(:, :, ii) = Te(:, :, ii) * hygrothermal.alpha';
-    hygrothermal.betaLocal(:, :, ii) = Te(:, :, ii) * hygrothermal.beta';
     
     % Free strain
     hygrothermal.freeStrain(:, :, ii) = ...
-        (hygrothermal.alphaLocal(:, :, ii) .* hygrothermal.deltaT) + ...
-        (hygrothermal.betaLocal(:, :, ii) .* hygrothermal.deltaC);
+        (hygrothermal.alphaLocal(:, :, ii) .* hygrothermal.deltaT);
     
 end
 
@@ -31,12 +25,17 @@ for ii = 2:length(ss) + 1
 
     hygrothermal.N = (hygrothermal.N + (Qbar(:, :, ii - 1) ...
         * hygrothermal.alphaLocal(:, :, ii - 1) .* (z(ii) - z(ii - 1)))); 
-    % ACCOUNT FOR MOISTURE TOO
     
-    hygrothermal.M = (hygrothermal.M + (Qbar(:, :, ii - 1) ...
-        * hygrothermal.betaLocal(:, :, ii - 1) .* ((z(ii) ^ 2) - ...
-        (z(ii - 1) ^ 2)))); % ACCOUNT FOR MOISTURE TOO
+    hygrothermal.M = (hygrothermal.M + (Qbar(:, :, ii - 1)));
 end
+
+N = hygrothermal.N
+M = hygrothermal.M
+% Hygrothermal global Strain
+
+
+
+% Hygrothermal global stress 
 
 end
 
