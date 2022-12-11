35f9b4a12c1384e554719475e1bc6ebdb28f62bb
Nick Agtual <nick.agtual@icloud.com>
2022-12-11 15:05:18 -0800

    Added tsai-wu faliure criterion nested function

--- Final Project/faliureCriteria.m
+++ Final Project/faliureCriteria.m
@@ -3,6 +3,7 @@ function [outputs] = faliureCriteria(strength, superimposedParam)
 criteria = {'maxStressCriterion', 'maxStrainCriterion', ...
     'tsaiHillCriterion', 'tsaiWuCriterion'};
 
+
 for ii = 1:length(criteria)
     
     faliure(1).criterion = criteria{ii};
@@ -55,10 +56,10 @@ end
                 strength.Ye)
             
             % Does laminate fail?
-            faliure(1).fail = 'Yes';
+            faliure(2).fail = 'Yes';
             
             % What is the mode of faliure?
-            faliure(1).mode = 'Fiber Faliure';
+            faliure(2).mode = 'Fiber Faliure';
             
         % Checking for matrix faliure (Method 1)
         elseif (min(superimposedParam.locStrain(1, 1, :)) ...
@@ -67,19 +68,19 @@ end
                 strength.YePrie)
             
             % Does laminate fail?
-            faliure(1).fail = 'Yes';
+            faliure(2).fail = 'Yes';
             
             % What is the mode of faliure?
-            faliure(1).mode = 'Matrix ';
+            faliure(2).mode = 'Matrix ';
             
         elseif abs(max(superimposedParam.locStrain(1, 3, :))) >= ...
                 strength.Se
             
             % Does laminate fail?
-            faliure(1).fail = 'Yes';
+            faliure(2).fail = 'Yes';
             
             % What is the mode of faliure?
-            faliure(1).mode = 'Matrix ';
+            faliure(2).mode = 'Matrix ';
             
         end 
     end
@@ -92,6 +93,34 @@ end
     function [out] = tsaiWuCriterion(strength, superimposedParam, ...
             faliure)
         
+        % Tsai-Wu criterion constants
+        constants.F11 = -1 / (strength.Xprime * strength.X);
+        constants.F1 = (1 / strength.X) + (1 / strength.Xprime);
+        constants.F22 = -1 / (strength.Yprime * strength.Y);
+        constants.F2 = (1 / strength.Y) + (1 / strength.Yprime);
+        constants.F12 = 1 / (2 * strength.Xprime * strength.X);
+        constants.F66 = 1 / strength.S;
+        
+        comparisonVar = ...
+            (constants.F1 * max(superimposed.locStress(1, 1, :))) + ...
+            (constants.F2 * max(superimposed.locStress(1, 2, :))) + ...
+            (constants.F11 * max(superimposed.locStress(1, 1, :))^ 2) + ...
+            (constants.F22 * max(superimposed.locStress(1, 2, :))^ 2) + ...
+            (2 * constants.F12 * max(superimposed.locStress(1, 1, :)) * ...
+            max(superimposed.locStress(1, 2, :))) + ...
+            (constants.F66 * max(superimposed.locStress(1, 3, :)) ^ 2);
+        
+        if comparisonVar >= 1
+            
+            % Does laminate fail?
+            faliure(4).fail = 'Yes';
+            
+            % What is the mode of faliure?
+            faliure(4).mode = 'Matrix ';
+            
+        end
+            
+
     end
 
 end
